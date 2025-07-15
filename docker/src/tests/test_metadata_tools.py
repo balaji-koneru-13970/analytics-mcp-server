f6bbd99ff614ff86736e1f761fd83b5f9169e4c7
Ashwin Prasad H <ashwin-17068@ashwin-17068.csez.zohocorpin.com>
2025-07-15 10:49:24 +0530

    Modified Test cases

--- docker/src/tests/test_metadata_tools.py
+++ docker/src/tests/test_metadata_tools.py
@@ -2,8 +2,16 @@ from tools.metadata_tools import get_workspaces_list, get_view_list
 import pytest
 
 @pytest.mark.parametrize("include_shared_workspaces, contains_str, expected", [
-    (False, 'Bugs Tracker Dataset', [{'workspaceId': '2588359000000656130', 'workspaceName': 'Bugs Tracker Dataset - Source', 'workspaceDesc': '', 'orgId': '784783856', 'createdTime': '1750049600716', 'createdBy': 'ashwinprasad.h@zohocorp.com', 'isDefault': False, 'owned': True}])
+    (False, 'Bugs Tracker Dataset', [{'workspaceName': 'Bugs Tracker Dataset - Source', 'workspaceDesc': '', 'isDefault': False, 'owned': True}])
 ])
 def test_get_workspaces(include_shared_workspaces, contains_str, expected):
     response = get_workspaces_list(include_shared_workspaces=include_shared_workspaces, contains_str=contains_str)
+    response.pop('orgId')
+    response.pop('createdBy')
+    response.pop('createdTime')
+    response.pop('workspaceId')
     assert response == expected
+
+
+def test_get_viewlist():
+    pass
